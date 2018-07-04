caabc456ca705efd6bbda330b5e2968de4a8f7f8
Stephen Bennett <stephen.bennett@dominos.com.au>
2018-07-05 09:10:50 +1000

    Comments and tidy up begins

--- email_main.py
+++ email_main.py
@@ -7,29 +7,27 @@ import settings
 import datetime
 
 def readConfiguration(configfile):
-    #global attachment_location, hive_address, hive_api
-
-    #print(str(datetime.datetime.now())+"  Using Config: {0}".format(configfile))
-
-	#Read in the configuration file called config.json
+	#Read in the configuration file that is called by email_main. e.g. python email_main.py --config=config.json
     try:
 		config = json.loads(open(configfile).read())
     except IOError:
         print(str(datetime.datetime.now())+"  ERROR: Failed to read in the ",configfile," file.")
         sys.exit()
 
-    #Read in config for the Email Servers (up to 5 mailservers defined)..yes I was lazy.
-    #Read config for default file location
+    #Read in the configuration file.
+    attachment_location = config['ATTACHMENTS']['location'] #Default location for attachments
+    hive_address = config['THEHIVE']['address'] #Address of TheHive server
+    hive_api = config['THEHIVE']['apikey'] #API Key used to access TheHive server
+    auto_create_tag = config['TAGS']['auto_create_tag'] #Tag that is detected in emails to automatically create a case
+    update_tag = config['TAGS']['update_tag'] #Tag that is detected in emails to automatically update existing cases
+    internal_email = config['INTERNAL_EMAIL']['server'] #The name of the internal email server to send emails
+    own_domain = config['INTERNAL_EMAIL']['own_domain'] #The email domain used by the company hosting TheHive
+    default_recipient = config['INTERNAL_EMAIL']['default_recipient'] #A default email recipient that will recieve emails
+    security_recipients = config['INTERNAL_EMAIL']['security_team'] #The list of recipients in the Security team to recieve emails
+    email_tag_line = config['INTERNAL_EMAIL']['email_tag'] #Standard message that will be included in each email.
+    remove_observables = config['OBSERVABLES']['remove_observables'] #List of observables to be ignored i.e. your internal email addressess and domains
 
-    attachment_location = config['ATTACHMENTS']['location']
-    hive_address = config['THEHIVE']['address']
-    hive_api = config['THEHIVE']['apikey']
-    auto_create_tag = config['TAGS']['auto_create_tag']
-    update_tag = config['TAGS']['update_tag']
-    internal_email = config['INTERNAL_EMAIL']['server']
-    own_domain = config['INTERNAL_EMAIL']['own_domain']
-    default_recipient = config['INTERNAL_EMAIL']['default_recipient']
-
+    #Store all of these variables in settings.py so that they can be used for all modules and python programs.
     settings.stored_hive_address.append(hive_address)
     settings.stored_api_key.append(hive_api)
     settings.stored_attachment_location.append(attachment_location)
@@ -38,33 +36,36 @@ def readConfiguration(configfile):
     settings.stored_internal_email.append(internal_email)
     settings.stored_own_domain.append(own_domain)
     settings.stored_def_recipient.append(default_recipient)
+    settings.stored_security_recipients.append(security_recipients)
+    settings.stored_email_tag_line.append(email_tag_line)
+    settings.stored_remove_observables.append(remove_observables)
 
     i=0
 
+    #I was lazy and put list of mailboxes to read in configuration file. Read them in here.
     for i in range(0,3):
-       mailbox=("mailbox{0}".format(i))
-       mail_server = config['MAILSERVER'][mailbox] #Name of the mailbox
-       mailbox=mail_server[0]
-       password=mail_server[1]
-       folder=mail_server[2]
-       tag=mail_server[3]
+        mailbox=("mailbox{0}".format(i))
+        mail_server = config['MAILSERVER'][mailbox] #Read in the name of the mailbox
+        mailbox=mail_server[0] 
+        password=mail_server[1]
+        folder=mail_server[2]
+        tag=mail_server[3]
 
-       email_ids, num_emails, emails = read_mailbox.connect_mailbox(mailbox,password,folder,tag)
+        #Execute function to read the mailbox and return the email ids, number collected and actual emails
+        email_ids, num_emails, emails = read_mailbox.connect_mailbox(mailbox,password,folder,tag)
 
-       if num_emails>0:
-#           I dont think it ever makes it here
-#           process_emails.email_process(emails,tag,attachment_location)
+
+        if num_emails>0:
+            #Not entirely sure why this is still here.
+            #process_emails.email_process(emails,tag,attachment_location)
             print("!!!I GOT HERE!!!")
-       else:
-            #print(str(datetime.datetime.now())+"  No emails to process")
-            print('')
 
 def main(argv):
     configfile = argv
-    readConfiguration(configfile)
+    readConfiguration(configfile) 
 
 if __name__ == '__main__':
-    settings.init()
+    settings.init() #Read in 'global' variable names
     parser = argparse.ArgumentParser(description='Optional config file.')
     parser.add_argument('--config',default='config.json', help="Add a config file path")
     inputvar = parser.parse_args()
--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+#This is used purely to store the contents of the configuration file in memory so that all modules can access these variables
 def init():
-    global stored_hive_address, stored_api_key, stored_attachment_location, stored_auto_create_tag, stored_update_tag, stored_internal_email, stored_own_domain, stored_def_recipient
+    global stored_hive_address, stored_api_key, stored_attachment_location, stored_auto_create_tag, stored_update_tag, stored_internal_email, stored_own_domain, stored_def_recipient, stored_security_recipients, stored_email_tag_line, stored_remove_observables
     stored_hive_address=[]
     stored_api_key=[]
     stored_attachment_location=[]
@@ -10,3 +11,6 @@ def init():
     stored_internal_email=[]
     stored_own_domain=[]
     stored_def_recipient=[]
+    stored_security_recipients=[]
+    stored_email_tag_line=[]
+    stored_remove_observables=[]
