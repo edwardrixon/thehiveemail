a4ddb685e881e1813316c68785415062f0976e1c
Stephen Bennett <stephen.bennett@dominos.com.au>
2018-07-05 13:20:55 +1000

    Streamline process and modules

--- modules.py
+++ modules.py
@@ -2,35 +2,26 @@
 
 #This is used to process all of thre messages that we just retrieved
 
-from __future__ import print_function
-from __future__ import unicode_literals
-from optparse import OptionParser
-
 import re
 import create_case
 import create_alert
 import extraction
-import os
-import mimetypes
-import requests
-import sys
-import json
-import uuid
-import time
-import imaplib
-import email
 import datetime
-import mailbox
-import html2text
 import settings
 import send_email
 
-#import mailparser
+def spam(email_message,subject,email_from,email_to,mailbox,password):
+    #This will handle any message sent to spam
+    template_name="SPAM INVESTIGATION"
+    case_tag="spam"
+    alert_pri = 1
+    print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
+    process_autocase(email_message,subject,template_name,case_tag,alert_pri,email_from,email_to,mailbox,password)
 
-def brand_monitor(email_message,subject):
+def brand_monitor(email_message,subject,email_from,email_to,mailbox,password):
     #This is to handle any alerts relating to brand monitoring in Domain Tools
     case_tag="Brand Alert"
-    template_name="" #Dont create a case  
+    template_name="" #Dont create a case
     print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
     if "Brand Monitor Alert" in subject:
         alert_title = "Domain Alert-Brand Alert"
@@ -46,44 +37,38 @@ def brand_monitor(email_message,subject):
     body=""
     create_alert.prepare_alert(subject,alert_pri,body,case_tag,alert_title)
 
-def email_pwned(email_message,subject,email_from,email_to,password):
+def email_pwned(email_message,subject,email_from,email_to,mailbox,password):
     #This is used for any emails picked up from the haveibeenpwned service
     case_tag="haveibeenpwned.com"
     template_name="USER INVESTIGATION"
     alert_pri = 2
     print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
-
     #We want to automatically create a case for this
-    case_id,simple_id,body = process_autocase(email_message,subject,template_name,case_tag)
-    send_email.send_mailbox(body,simple_id,email_from, email_to, subject,mailbox,password) 
+    process_autocase(email_message,subject,template_name,case_tag,alert_pri,email_from,email_to,mailbox,password)
 
-def pastebin_alert(email_message,subject):
+def pastebin_alert(email_message,subject,email_from,email_to,mailbox,password):
     #This is used for any pastebin alerts.
     case_tag="Pastebin Alert"
     template_name=""
     alert_pri = 2
     print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
+    process_autocase(email_message,subject,template_name,case_tag,alert_pri,email_from,email_to,mailbox,password)
 
-def process_autocase(email_message,subject,template_name,case_tag):
+#Process any that will automatically generate a case in TheHive. These will always send an email.
+def process_autocase(email_message,subject,template_name,case_tag,alert_pri,email_from,email_to,mailbox,password):
     #This process is kicked off when you send a message to the identified mailbox with CASE in the subject
-
-    #raw_email = mailparser.parse_from_string(email_message)   
+    print(str(datetime.datetime.now())+"  Processing auto case creation for "+template_name+" with tag "+case_tag)
     attachment_location=''.join(settings.stored_attachment_location[0])
-    
-    #email_message = raw_email
-
     subject = subject.decode('unicode_escape').encode('utf-8')
-#    print("DECODED SUBJECT:",subject)
-
     body, url_array, mail_array = extraction.extractbody(email_message)
     file_array = extraction.extractattachments(email_message,attachment_location)
     TemplateName=template_name
-    id, simple_id, simple_body = create_case.prepare_case_template(subject,1,body,"AutoCreated",case_tag,TemplateName)
+    id, simple_id, simple_body = create_case.prepare_case_template(subject,alert_pri,body,"AutoCreated",case_tag,TemplateName)
     create_case.prepare_mail_observable(id, mail_array)
     create_case.prepare_url_observable(id, url_array)
     create_case.prepare_file_observable(id, file_array)
     #file_array = extraction.extractattachments(email_message,attachment_location)
-    return simple_id, simple_body
+    send_email.send_mailbox(body,simple_id,email_from, email_to, subject,mailbox,password)
 
 def update_autocase(email_message,subject):
     update_tag=''.join(settings.stored_update_tag[0])
@@ -99,6 +84,5 @@ def update_autocase(email_message,subject):
     file_array = extraction.extractattachments(email_message,attachment_location)
 
     #We need to add something to the task called History that we have setup
-    TemplateName="AUTOCASE"
     create_case.search_case(id,body,file_array)
 
--- process_emails.py
+++ process_emails.py
@@ -9,12 +9,11 @@ import datetime
 import send_email
 import settings
 
+#This module will process the email sent to it and select relevant modules to execute.
 def email_process(emails,tag,mailbox,password):
-     #raw_email = mailparser.parse_from_string(emails)
-     update_tag=''.join(settings.stored_update_tag[0])
-     auto_create_tag=''.join(settings.stored_auto_create_tag[0])
-     email_message = emails
-
+    update_tag=''.join(settings.stored_update_tag[0])
+    auto_create_tag=''.join(settings.stored_auto_create_tag[0])
+    email_message = emails
 
      #Extract the Date -Not used yet
      #date_tuple = email.utils.parsedate_tz(emails['Date'])
@@ -25,43 +24,35 @@ def email_process(emails,tag,mailbox,password):
      #   local_message_date = "%s" %(str(local_date.strftime("%a, %d %b %Y %H:%M:%S")))
 
      #Common Extractions for all discovered emails to be searched on
-     email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
-     email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
-     subject = decode_header(email_message.get('subject'))[0]
+    email_from = str(email.header.make_header(email.header.decode_header(email_message['From'])))
+    email_to = str(email.header.make_header(email.header.decode_header(email_message['To'])))
+    subject = decode_header(email_message.get('subject'))[0]
 
-     if tag=="spam":
-        #Process messages from the spam mailbox. These will always create a case, extract attachments etc.
+     #If the email has the autocase trigger in it then regardless of which mailbox it came from.
+    if update_tag in subject:
+        modules.update_autocase(email_message,subject) #run module to update an existing case.
+    elif auto_create_tag in subject:
+        template_name="AUTOCASE"
+        case_tag="Auto created general case"
+        alert_pri = 1
+        modules.process_autocase(email_message,subject,template_name,case_tag,alert_pri,email_from,email_to,mailbox,password)
         
-        if update_tag in subject:
-            modules.update_autocase(email_message,subject)
-        else:
-            template_name="SPAM INVESTIGATION"
-            case_tag="spam"
-            print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
-            simple_id,body = modules.process_autocase(email_message,subject,template_name,case_tag)
-            send_email.send_mailbox(body,simple_id,email_from, email_to, subject,mailbox,password)
-     
-     elif tag=="security":
-        #Process messages from the security mailbox. Case extractions depends on what the subject is etc
-        if auto_create_tag in subject:
-         #This is just a test, eventually all emails will need an autoresponse
-            template_name="AUTOCASE"
-            case_tag=""
-            print(str(datetime.datetime.now())+"  Processing "+template_name+" with tag "+case_tag)
-            case_id,simple_id,body = modules.process_autocase(email_message,subject,template_name,case_tag)
-            send_email.send_mailbox(body,simple_id,email_from, email_to, subject,mailbox,password)
-        elif update_tag in subject:
-           modules.update_autocase(email_message,subject)   
-        elif "noreply@haveibeenpwned.com" in email_from and "multi-domain" not in subject:
-            modules.email_pwned(email_message,subject,email_from,email_to,password)
-            send_email.send_mailbox(body,simple_id,email_from, email_to, subject,mailbox,password)
-        elif "domaintools.com" in email_from:
-            modules.brand_monitor(email_message,subject)
-        else:
-            #If it gets to here unset the read flag (so we know whats been read but not processed) - NOT IMPLEMENTED YET
-            #result, email_data = mail.uid('store',latest_email_uid,'-FLAGS','\\Seen')
-            print("")
-     else:
+    #Process messages tagged as spam. These will always create a case, extract attachments etc.
+        if tag=="spam":
+            modules.spam(email_message,subject,email_from,email_to,mailbox,password)
+
+    #Process messages tagged as security. This will call modules that determines what happens next based on search criteria
+        elif tag=="security":
+            #Process messages from the security mailbox. Case extractions depends on what the subject is etc
+            if "noreply@haveibeenpwned.com" in email_from and "multi-domain" not in subject:
+                modules.email_pwned(email_message,subject,email_from,email_to,mailbox,password) #
+            elif "domaintools.com" in email_from:
+                modules.brand_monitor(email_message,subject,email_from,email_to,mailbox,password)
+            else:
+                #If it gets to here unset the read flag (so we know whats been read but not processed) - NOT IMPLEMENTED YET
+                #result, email_data = mail.uid('store',latest_email_uid,'-FLAGS','\\Seen')
+                print("")
+    else:
         print(str(datetime.datetime.now())+"  No TAGs have been applied to the email")
 
 
